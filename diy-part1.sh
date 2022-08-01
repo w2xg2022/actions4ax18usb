9b44b1b3177353618e8edba8dd8b05240fa838d5
w2xg2022 <110317133+w2xg2022@users.noreply.github.com>
2022-08-01 09:12:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git nuexini https://github.com/NueXini/NueXini_Packages' >>feeds.conf.default
