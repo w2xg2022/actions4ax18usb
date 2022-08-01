3e0d070f77e09d139fedfa9f9688ba2565dae657
w2xg2022 <110317133+w2xg2022@users.noreply.github.com>
2022-08-01 09:20:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default hostname
+#sed -i 's/OpenWrt/WKN/g' package/base-files/files/bin/config_generate
