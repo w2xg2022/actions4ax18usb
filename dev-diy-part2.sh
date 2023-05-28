82937af89ff5c136f15d3b91b9e0f59235f15705
w2xg2022 <w2xg2022@163.com>
2023-05-28 09:31:43 +0800

    Test usb driver

---
+++ dev-diy-part2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default static IP
+#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+
+# Modify default hostname
+sed -i 's/OpenWrt/AX18USB/g' package/base-files/files/bin/config_generate
+
+# Modify default timezone & ntp server
+sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
+sed -i 's/0.openwrt.pool.ntp.org/0.ntp.aliyun.com/g' package/base-files/files/bin/config_generate
